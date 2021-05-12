86946fc7cdf72faecf58ef7bb2ff5d13db418a03
Vitaliy Tomin <highwaystar.ru@gmail.com>
2021-05-12 15:48:10 +0800

    x705: Build android.frameworks.displayservice@1.0 need for camera blob

--- device.mk
+++ device.mk
@@ -37,6 +37,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1
 
+# Display
+PRODUCT_PACKAGES += \
+    android.frameworks.displayservice@1.0
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
